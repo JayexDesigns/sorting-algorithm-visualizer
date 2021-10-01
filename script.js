23d058c850951860b545c77864229c1971f30a0c
Jayex <joaquintouron@gmail.com>
2021-10-02 00:50:00 +0200

    first version of sorting algorithm visualizer

---
+++ script.js
@@ -0,0 +1,93 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+
+
+const createArray = (size) => {
+    let elements = [];
+    for (let i = 0; i < size; ++i) {
+        elements.push(i);
+    }
+    return elements;
+}
+
+const shuffleArray = (elements) => {
+    let currentIndex = elements.length;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        let tmp = elements[currentIndex];
+        elements[currentIndex] = elements[randomIndex];
+        elements[randomIndex] = tmp;
+    }
+    return elements;
+}
+
+const getHighestValue = (elements) => {
+    let maxValue;
+    for (let i = 0; i < elements.length; ++i) {
+        if (maxValue == undefined || maxValue < elements[i]) maxValue = elements[i];
+    }
+    return maxValue;
+}
+
+
+
+const draw = async (elements) => {
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    let separationX = innerWidth/elements.length;
+    let separationY = innerHeight/getHighestValue(elements);
+    for (let i = 0; i < elements.length; ++i) {
+        ctx.beginPath();
+        ctx.fillStyle = "#ffffff";
+        ctx.arc(
+            i*separationX,
+            innerHeight-elements[i]*separationY,
+            1,
+            0, Math.PI*2, true
+        );
+        ctx.fill();
+    }
+    return 1;
+}
+
+
+
+
+
+var state = {sorting: false};
+var elements;
+const start = () => {
+    elements = createArray(100);
+    elements = shuffleArray(elements);
+    draw(elements);
+}
+
+var buttons = document.getElementsByClassName("control-button");
+for (let i = 0; i < buttons.length; ++i) {
+    buttons[i].addEventListener('click', () => {
+        switch (buttons[i].innerText) {
+            case "SHUFFLE":
+                elements = shuffleArray(elements);
+                state.sorting = false;
+                draw(elements);
+                break;
+            case "BUBBLE":
+                state.sorting = true;
+                bubbleSort(state, elements, draw);
+                break;
+        }
+    });
+}
+
+
+
+
+
+const resizeCanvas = () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    start();
+}
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
---
+++ sort.js
@@ -0,0 +1,15 @@
+const bubbleSort = async (state, elements, callback) => {
+    for (let i = 1; i < elements.length; ++i) {
+        for (let j = 0; j < elements.length-1; ++j) {
+            if (state.sorting == false) return;
+            if (elements[j] > elements[j+1]) {
+                let tmp = elements[j];
+                elements[j] = elements[j+1];
+                elements[j+1] = tmp;
+            }
+            await callback(elements);
+        }
+        await sleep(1);
+    }
+    state.sorting = false;
+}
